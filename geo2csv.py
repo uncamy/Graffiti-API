82818a0fa01e68a1a9d26b972ba4c1011d2cf5f5
Amy Roberts <uncamy@gmail.com>
2013-11-19 18:59:54 -0500

    final csv files read in one at a time (7 files total ~100 obs each)

--- geo2csv.py
+++ geo2csv.py
@@ -6,35 +6,33 @@ from time import sleep
 
 graffiti = pd.read_csv('./Graffiti_Locations.csv')
 
+
 #geocode data with pauses
 def geocode(address):
-    lat_lon = Geocoder.geocode(address)
-    sleep(2)
-    return(lat_lon[0].coordinates)
+        lat_lon = Geocoder.geocode(address)
+        sleep(2)
+        return(lat_lon[0].coordinates)
+
 
 #setting up data:
 #filter by bourough to only get Manhattan Graffiti that is 'open'
 manhattan = graffiti[graffiti['Borough']== 'MANHATTAN']
-current_graffiti = manhattan[manhattan['Status']== 'Open']
+current_graffiti = manhattan[manhattan['Status']== 'Open'][600:]
 
 #add new column for the address
 current_graffiti['full_address'] = \
         current_graffiti['Incident Address Display'] + ', NEW YORK, NEW YORK'
 
 
-#geocode data and catch errors
+#geocode data
 def address2geocode():
     print 'starting geocoding'
-    try:
-        current_graffiti['geocode'] = \
-                current_graffiti['full_address'].map(geocode)
-    except:
-        current_graffiti['geocode'] = None
+    current_graffiti['geocode']= current_graffiti['full_address'].map(geocode)
     print 'end geocoding'
 
 address2geocode()
-current_graffiti.to_csv('graffiti_geocode.csv')
-
+current_graffiti.to_csv('graffiti_geocode7.csv')
+df= pd.read_csv('./graffiti_geocode7.csv')
 
 '''
 reduced_dataset = address[:10]
