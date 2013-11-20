1fba6532be9fa7baef0dfe7eab2ba96995870e8e
Amy Roberts <uncamy@gmail.com>
2013-11-20 13:31:33 -0500

    added marker for location

---
+++ static/map.js
@@ -0,0 +1,23 @@
+/*javascript needed for google maps rendering */
+
+var latlng= new google.maps.LatLng(40.72078, -74.001119);
+
+function initialize(){
+    var mapOptions = {
+          center: latlng,y
+          zoom: 15
+    }
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+
+    /*adds a marker to the map for the user's location*/
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "user location"
+    });
+
+
+}
+google.maps.event.addDomListener(window, 'load', initialize);
