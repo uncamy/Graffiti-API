b2eaf1765526e3f7fc4784ca07ec0001b6932932
Amy Roberts <uncamy@gmail.com>
2013-11-20 17:29:32 -0500

    added second marker for graffiti awith image

--- static/map.js
+++ static/map.js
@@ -16,8 +16,23 @@ function initialize(){
     /*adds a marker to the map for the user's location*/
     var marker = new google.maps.Marker({
         position: latlng,
-        map: map,
-        title: "user location"
+        title: "user location",
+        map: map
+    });
+
+    /*add marker for graffiti locations */
+    var image = {
+        url:'http://www.alispagnola.com/Free/spraypaint2.jpg',
+        scaledSize: new google.maps.Size(32, 32),
+        origin: new google.maps.Point(0,0)
+    };
+
+    var location = new google.maps.LatLng(40.7236264, -73.9980549);
+    var graffiti_marker = new google.maps.Marker({
+        icon: image,
+        position: location,
+        title: "graffiti",
+        map: map
     });
 
 
