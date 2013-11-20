31c3ab986cc8cb17e3b3e3bb06947e358439a396
Amy Roberts <uncamy@gmail.com>
2013-11-19 21:57:29 -0500

    basic app running

--- graffiti.py
+++ graffiti.py
@@ -3,32 +3,37 @@ import sqlite3
 import string
 import pandas as pd
 
+from geopy.geocoders import GoogleV3
+from geopy.distance import vincenty
+from googlemap import GoogleMaps
+import api_key
+
 from pygeocoder import Geocoder
 from flask  import Flask, request, session, g, redirect, url_for,\
                   abort, render_template, flash, json
 from flask.ext import restful
 from contextlib import closing
 
+
 #configuration
-DATABASE = 'data_graffiti.db'
 DEBUG = True
-SECRET_KEY = 'graffiti_key'
-SCHEMA = 'schema.sql'
-CSV = 'Graffiti_Locations.csv'
+CSV = './data/geocoded_manhattan.csv'
+df = pd.read_csv(CSV)
+gmaps = GoogleMaps(api_key)
 
 app = Flask(__name__)
-api = restful.Api(app)
-
+app.config.from_object(__name__)
 
 def geocode(address):
     lat_lon = Geocoder.geocode(address)
     return(lat_lon[0].coordinates)
 
-print(geocode('455 Broadway, New York, NY 10013'))
 
-def convert_geocode(address):
-    geocode(address)
-    x= lat_lon[0]
-    y= lat_lon[1]
-    print x
-    print y
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        address = request.form['address']
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
