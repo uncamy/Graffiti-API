9a67193069e60e40aa8b1d2446cc4cc2a8cf6e64
Amy Roberts <uncamy@gmail.com>
2013-11-21 12:23:51 -0500

    added equalities and new variable to pass graffiti locations to index

--- graffiti.py
+++ graffiti.py
@@ -24,7 +24,21 @@ API_KEY = './api_key.txt'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+#process data to allow for calculations on lat/lng
+def fix_geocodes(sample):
+    sample = sample.replace("(", "")
+    sample = sample.replace(")", "")
+    sample = sample.split(',')
+    lat  = float(sample[0])
+    lng = float(sample[1])
+    new_geo= (lat, lng)
+    return new_geo
 
+geo_pairs= df['geocode'].map(fix_geocodes)
+df['lat'] = geo_pairs.map(lambda x: x[0])
+df['lng'] = geo_pairs.map(lambda x: x[1])
+
+#df.to_csv('./data/final_data.csv')
 
 def geocode(address):
     try:
@@ -33,33 +47,37 @@ def geocode(address):
     except:
         return 'unable to geocode'
 
-
-
-
 #test address
-address = '455 Broadway, New York, NY'
 def locations(address):
     geo_loc = geocode(address)
     return geo_loc
 
 def vicinity(geo_loc):
 #there must be a better way to do this...
+    distance = 0.000001
     lat = geo_loc[0]
     lng = geo_loc[1]
-    lat_max = lat + 0.000001
-    lat_min = lat - 0.000001
-    lng_max = lng + 0.000001
-    lng_min = lng - 0.000001
+    lat_max = lat + distance
+    lat_min = lat - distance
+    lng_max = lng + distance
+    lng_min = lng - distance
 
-    #df[df['lng']]
+    #equalities
+    nearby = df[df['lng']> lng_min] and df[df['lng'] <lng_max] \
+             and df[df['lat']> lat_min] and df[df['lng'] <lng_max]
+    graffiti= nearby['geocode']
+    return graffiti_loc
+
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
         address = request.form['address']
-        find_loc = locations(address)
-        return render_template('index.html', graffiti = find_loc)
+        user_loc = locations(address)
+        graffiti_loc = vicinity(user_loc)
+        return render_template('index.html', your_loc = user_loc,\
+                               graffiti = graffiti_loc)
     else:
         return render_template('index.html')
 
