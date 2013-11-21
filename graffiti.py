a31f03266ff00c07c1d570ddeeff3aa9f8d82e45
Amy Roberts <uncamy@gmail.com>
2013-11-20 20:21:11 -0500

    coverted geocodes to floats

--- graffiti.py
+++ graffiti.py
@@ -24,13 +24,45 @@ API_KEY = './api_key.txt'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def geocode(address):
-    lat_lon = Geocoder.geocode(address)
-    return(lat_lon[0].coordinates)
+def fix_geocodes(sample):
+    print type(sample)
+    sample = sample.replace("(", "")
+    sample = sample.replace(")", "")
+    sample = sample.split(',')
+    lat  = float(sample[0])
+    lng = float(sample[1])
+    new_geo= (lat, lng)
+    return new_geo
 
+geo_pairs= df['geocode'].map(fix_geocodes)
+df['lat'] = geo_pairs.map(lambda x: x[0])
+df['lng'] = geo_pairs.map(lambda x: x[1])
+
+def geocode(address):
+    try:
+        lat_lon = Geocoder.geocode(address)
+        return(lat_lon[0].coordinates)
+    except:
+        return 'unable to geocode'
+
+
+#test address
+address = '455 Broadway, New York, NY'
 def locations(address):
     geo_loc = geocode(address)
     return geo_loc
+
+def vicinity(geo_loc):
+#there must be a better way to do this...
+    lat = geo_loc[0]
+    lng = geo_loc[1]
+    lat_max = lat + 0.000001
+    lat_min = lat - 0.000001
+    lng_max = lng + 0.000001
+    lng_min = lng - 0.000001
+
+    #df[df['lng']]
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
